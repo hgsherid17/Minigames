ec739bb65f4bb725b236372b39a13a1e47b4b6ac
hannah <hgsherid@uvm.edu>
2023-04-11 17:55:46 -0400

    Printing score

--- graphics.cpp
+++ graphics.cpp
@@ -6,16 +6,27 @@
 #include "circle.h"
 #include "shape.h"
 #include "quad.h"
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 GLdouble width, height;
 Circle wheel;
+
+/* Pong */
 Quad leftBar;
 Quad rightBar;
 Circle ball;
 Quad colorBox;
+int leftScore;
+int rightScore;
+const int SCORE_POS_X = 100;
+const int SCORE_POS_Y = 50;
+
+
 float rotationAngle = 0.0f;
+const int PENCIL_SIZE = 10;
 
 bool spinning = false;
 
@@ -49,6 +60,9 @@ void initPongBars() {
     rightBar.setColor(white);
     leftBar.setSize(10, 70);
     rightBar.setSize(10, 70);
+
+    leftScore = 0;
+    rightScore = 0;
 }
 void initPongBall() {
     ball.setColor(white);
@@ -79,24 +93,90 @@ void moveBall(int val) {
     // Ball hits top
     if (ball.getTopY() < (ball.getRadius() * 2)) {
         ball.bounceY();
+        if (leftScore == 5) {
+            leftScore = 0;
+            rightScore = 0;
+        }
+        else {
+            ++leftScore;
+        }
+
         //ball.setCenterY(ball.getRadius());
     }
     // Ball hits bottom
     else if (ball.getBottomY() > height) {
         ball.bounceY();
+        if (leftScore == 5) {
+            leftScore = 0;
+            rightScore = 0;
+        }
+        else {
+            ++leftScore;
+        }
         //ball.setCenterY(height - ball.getRadius());
 
     }
     else if (ball.getLeftX() < (ball.getRadius() * 2)) {
         ball.bounceX();
+        if (rightScore == 5) {
+            leftScore = 0;
+            rightScore = 0;
+        }
+        else {
+            ++rightScore;
+        }
         //ball.setCenterX(ball.getRadius());
     }
     else if (ball.getRightX() > width) {
         ball.bounceX();
+        if (rightScore == 5) {
+            leftScore = 0;
+            rightScore = 0;
+        }
+        else {
+            ++rightScore;
+        }
         //ball.setCenterX(width - ball.getRadius());
     }
 
     glutPostRedisplay();
+}
+void showPongScore(string filename, int x, int y) {
+    ifstream inFile("../" + filename);
+    inFile >> noskipws;
+    int xCoord = x;
+    int yCoord = y;
+    char letter;
+    bool draw;
+    while (inFile >> letter) {
+        draw = true;
+        switch(letter) {
+            case 'r': glColor3f(1, 0, 0); break;
+            case 'g': glColor3f(0, 1, 0); break;
+            case 'b': glColor3f(0, 0, 1); break;
+            case 'y': glColor3f(1, 1, 0); break;
+            case 'm': glColor3f(1, 0, 1); break;
+            case 'c': glColor3f(0, 1, 1); break;
+            case 't': glColor3f(0, 0.5, 0.5); break;
+            case 'p' : glColor3f(0.5, 0, 0.5); break;
+            case 'w' : glColor3f(1, 1, 1); break;
+            case ' ': glColor3f(0.137255, 0.556863, 0.419608); break; //seaGreen
+            default: // newline
+                draw = false;
+                xCoord = x;
+                yCoord += PENCIL_SIZE;
+        }
+        if (draw) {
+            glBegin(GL_QUADS);
+            glVertex2i(xCoord, yCoord);
+            glVertex2i(xCoord+PENCIL_SIZE, yCoord);
+            glVertex2i(xCoord+PENCIL_SIZE, yCoord+PENCIL_SIZE);
+            glVertex2i(xCoord, yCoord+PENCIL_SIZE);
+            glEnd();
+            xCoord += PENCIL_SIZE;
+        }
+    }
+    inFile.close();
 }
 void spinWheel(int val) {
     /**
@@ -143,6 +223,8 @@ void display() {
         rightBar.draw();
         ball.draw();
         moveBall(0);
+        showPongScore(to_string(leftScore) + ".txt", SCORE_POS_X, SCORE_POS_Y);
+        showPongScore(to_string(rightScore)+".txt", width - SCORE_POS_X, SCORE_POS_Y);
     }
 
     glFlush();
